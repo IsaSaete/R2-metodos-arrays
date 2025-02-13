1d82066c9e9a8e5fce21933fb8dd372bd5447d5a
Isabel Sáenz <isasaete@gmail.comn>
2025-02-13 18:06:56 +0100

    doing the exercise using methods

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-let years: number[] = [];
+/*let years: number[] = [];
 const actualYear = 2024;
 
 for (let yearsCount = 2000; yearsCount <= actualYear; yearsCount++) {
@@ -46,35 +46,39 @@ const words = [
   "diez",
   "Con",
 ];
-console.log(words.reverse().join(" "));
+console.log(words.reverse().join(" ")); */
+
+// Crea una variable que almacenará las notas de un examen. Créala primero con
+// y un array vacío, y después asígnale un array con 5 notas.>>LISTO
+// Crea una nueva variable y asígnale las notas anteriores pero ordenadas de menor a mayor.>>LISTO
+// Imprime por consola las notas que han sido aprobadas (5 o más).
+// Imprime por consola la nota media.
+// Imprime por consola la nota más alta y la nota más baja.
 
 let scores: number[] = [];
 scores = [3, 7, 9, 5, 6];
 
-let orderedScores = scores.sort();
-console.log(orderedScores);
+let orderedScores = scores.sort((a, b) => {
+  return a - b;
+});
 
-for (const position in scores) {
-  const allScores = scores[position];
-  if (allScores >= 5) {
-    console.log(allScores);
-  }
-}
+let aprovedScores = scores.filter((score) => {
+  return score >= 5;
+});
+console.log(`The approved scores are: ${aprovedScores}`);
 
-let averageScores = 0;
-for (const position in scores) {
-  averageScores = averageScores + scores[position];
-}
-console.log(`The average score is: ${averageScores / scores.length}`);
+let sumScores = scores.reduce((a, b) => {
+  return a + b;
+}, 0);
+let averageScores = sumScores / scores.length;
+console.log(`The average scores is: ${averageScores}`);
 
-let higuestScores = scores[0];
-for (const position in scores) {
-  if (higuestScores < scores[position]) higuestScores = scores[position];
-}
-console.log(`The highest score is: ${higuestScores}`);
+let maximumScores = scores.reduce((a, b) => {
+  return a > b ? a : b;
+});
+console.log(`The highest score is: ${maximumScores}`);
 
-let lowestScores = scores[0];
-for (const position in scores) {
-  if (lowestScores > scores[position]) lowestScores = scores[position];
-}
+let lowestScores = scores.reduce((a, b) => {
+  return a < b ? a : b;
+});
 console.log(`The lowest score is: ${lowestScores}`);
